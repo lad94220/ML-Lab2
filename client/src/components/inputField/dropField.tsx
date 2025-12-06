7c406c92f9490e68f2dde8ca42d5f39ccaf4e4eb
lad94220 <laduy231@clc.fitus.edu.vn>
2025-12-06 23:09:28 +0700

    add drop field

---
+++ client/src/components/inputField/dropField.tsx
@@ -0,0 +1,48 @@
+import Dropzone from "dropzone";
+import "dropzone/dist/dropzone.css";
+import { useEffect, useRef } from "react";
+
+Dropzone.autoDiscover = false;
+
+export const DropField = () => {
+  const dropzoneRef = useRef<HTMLFormElement>(null);
+
+  useEffect(() => {
+    if (!dropzoneRef.current) return;
+
+    const dropzone = new Dropzone(dropzoneRef.current, {
+      url: "/file/post",
+      maxFiles: 1,
+      acceptedFiles: "image/*",
+      addRemoveLinks: true,
+      dictDefaultMessage: "Drop files here or click to upload",
+    }); 
+
+    return () => {
+      dropzone.destroy();
+    }
+  }, []);
+
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-center text-2xl font-semibold pb-8">
+        INPUT FILES
+      </div>
+      <form 
+        ref={dropzoneRef} 
+        className="dropzone my-custom-class border p-4 rounded-lg"
+        >
+        <div className="dz-message">
+          Drop files here or click to upload
+        </div>
+      </form>
+      <div className="flex justify-between relative mb-8">
+        <button
+          className="px-4 py-1 absolute right-0 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-600 transition-colors"
+        >
+          Recognize
+        </button>
+      </div>
+    </div>
+  )
+}
